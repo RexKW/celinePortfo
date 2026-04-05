e744d59f4f547610507f3fc60233af1c73f916f2
Rex Kenny Wirasantoso <153161558+RexKW@users.noreply.github.com>
2026-04-05 16:34:17 +0700

    added personal works assets

--- app/components/portfolioLayouts/personalWorks.tsx
+++ app/components/portfolioLayouts/personalWorks.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { grenzeGotisch } from '../../fonts'
+import Image from 'next/image'
 
 
 export default function PersonalWorks() {
@@ -31,25 +32,26 @@ export default function PersonalWorks() {
                 </div>
 
                 {/* Image */}
-                <img
-                    src=""
+                <Image width={600} height={300}
+                    id='borderimg'
+                    src="/portfolio/personalWorks/personalWorks1.png"
                     alt=""
-                    className='bg-white w-[300px] md:w-[600px] h-[200px] md:h-[400px] order-2 md:order-2'
+                    className='bg-white w-[300px] md:w-[600px] h-auto order-2 md:order-2'
                 />
             </div>
-            <div className='flex flex-col justify-center items-center  justify-items-center w-full grid grid-cols-1 md:grid-cols-2 gap-10 mt-20'>
-                <img src="" className='w-full h-[300px] bg-white order-1' alt="" />
-                <img src="" className='w-full h-[300px] bg-white order-2' alt="" />
-                <img src="" className='w-full h-[300px] bg-white order-3 md:order-4' alt="" />
+            <div className='flex flex-col justify-center items-center  justify-items-center w-full grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 mt-20'>
+                <Image width={600} height={300} src="/portfolio/personalWorks/personalWorks2.png" className='w-full h-auto  order-1' alt="" />
+                <Image width={600} height={300} src="/portfolio/personalWorks/personalWorks3.png" className='w-full h-auto bg-white order-2' alt="" />
+                <Image width={600} height={300} src="/portfolio/personalWorks/personalWorks4.png" className='w-full h-auto bg-white order-3 md:order-4' alt="" />
                 <div className='order-4 md:order-3 w-[600px] flex justify-start w-full md:pr-20 items-end'>
                     <p className='text-2xl text-justify'>Through their journeys, relationships, and personal loss, the story explores themes of humanity, freedom, and the meaning of moving forward despite the weight of the past. <br /><br />Many of my illustrations are scences from this project, focusing on character emotion,  interactions, and the histories of the people within this world.</p>
                 </div>
             </div>
             <div className='flex grid grid-cols-1  justify-items-center w-ful1 md:grid-cols-2 gap-10 mt-20'>
-                <img src="" className='w-full h-[300px] bg-white order-1' alt="" />
-                <img src="" className='w-full h-[300px] bg-white order-2' alt="" />
-                <img src="" className='w-full h-[300px] bg-white order-3' alt="" />
-                <img src="" className='w-full h-[300px] bg-white order-4' alt="" />
+                <img src="/portfolio/personalWorks/personalWorks5.png" className='w-full h-auto order-1' alt="" />
+                <img src="/portfolio/personalWorks/personalWorks6.png" className='w-full h-auto order-2' alt="" />
+                <img src="/portfolio/personalWorks/personalWorks7.png" className='w-full h-auto order-3' alt="" />
+                <img src="/portfolio/personalWorks/personalWorks8.png" className='w-full h-auto order-4' alt="" />
             </div>
         </div>
     )
