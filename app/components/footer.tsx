6e7edcd6ae355230632f17b9786459c078aa0859
Rex Kenny Wirasantoso <153161558+RexKW@users.noreply.github.com>
2026-04-14 23:24:24 +0700

    added vertical padding to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ import { grenzeGotisch, pirataOne } from '../fonts'
 
 function Footer() {
   return (
-    <div id='contact' className='min-h-[50vh] min-w-screen relative overflow-hidden z-100 text-white bg-[#ffffff] bg-[radial-gradient(circle_at_0%_0%,#111625,transparent_65%),radial-gradient(circle_at_25%_0%,#253c3f,transparent_75%),radial-gradient(circle_at_25%_75%,#486664,transparent_80%),radial-gradient(circle_at_75%_0%,#173a50,transparent_65%),radial-gradient(circle_at_100%_0%,#122b4d,transparent_65%),radial-gradient(circle_at_50%_50%,#325153,transparent_65%),radial-gradient(circle_at_0%_100%,#131724,transparent_65%),radial-gradient(circle_at_100%_100%,#122b4d,transparent_85%),radial-gradient(circle_at_100%_0%,#113a5c,transparent_85%)] flex justify-center items-center'>
+    <div id='contact' className='min-h-[50vh] min-w-screen relative overflow-hidden z-100 text-white bg-[#ffffff] bg-[radial-gradient(circle_at_0%_0%,#111625,transparent_65%),radial-gradient(circle_at_25%_0%,#253c3f,transparent_75%),radial-gradient(circle_at_25%_75%,#486664,transparent_80%),radial-gradient(circle_at_75%_0%,#173a50,transparent_65%),radial-gradient(circle_at_100%_0%,#122b4d,transparent_65%),radial-gradient(circle_at_50%_50%,#325153,transparent_65%),radial-gradient(circle_at_0%_100%,#131724,transparent_65%),radial-gradient(circle_at_100%_100%,#122b4d,transparent_85%),radial-gradient(circle_at_100%_0%,#113a5c,transparent_85%)] py-10 flex justify-center items-center'>
       <img src="/footer/FooterStars.svg" className='absolute  w-full h-full object-cover z-0 left-0' alt="" />
       <img src="/footer/footerGrain_1.png" className='absolute w-full h-full z-1 left-0 mix-blend-difference ' alt="" />
       <div className='flex flex-col h-full w-full'>
